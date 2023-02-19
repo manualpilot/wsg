4bc40c065961eb03cad1db0d3d8f6b481dd2e563
sep <sepbot@gmail.com>
2023-02-19 22:25:12 +1100

    exec

--- .deploy/start.sh
+++ .deploy/start.sh
@@ -3,4 +3,4 @@
 export INSTANCE_ID="${FLY_ALLOC_ID}"
 
 # TODO: can we tell paketo to put the binary in a less suspicious location
-/layers/paketo-buildpacks_go-build/targets/bin/wsg
+exec /layers/paketo-buildpacks_go-build/targets/bin/wsg
